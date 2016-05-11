fc251e2396b46ae461998106b56d1aa4f70f6c26
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-05-12 00:58:46 +0530

    code indented

--- TCPClient.java
+++ TCPClient.java
@@ -5,145 +5,127 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class TCPClient extends JFrame implements ActionListener{
-	JPanel panel;
-	JLabel title,subT;
-	Font font;
-	JTextField txt;
-	JButton up,down;
-	String dirName = "c:/FTP CLIENT DIRECTORY";
-	Socket clientSocket; 
-	InputStream inFromServer ;
-	OutputStream outToServer;
-	PrintWriter pw;
-	String name, file;
-	int c;
-	int size = 9022386;
-	public TCPClient(){
-		super("TCP CLIENT");
-		panel = new JPanel(null);
-		font = new Font("Roboto",Font.BOLD,60);
-		title = new JLabel("TCP CLIENT");
-		title.setFont(font);
-		title.setBounds(300,100,400,100);
-		panel.add(title);
-		subT = new JLabel("Enter File Name :");
-		subT.setBounds(100,300,200,100);
-		panel.add(subT);
-		txt = new JTextField();
-		txt.setBounds(400,300,500,100);
-		panel.add(txt);
-		up = new JButton("Upload");
-		up.setBounds(250,500,200,100);
-		panel.add(up);
-		down = new JButton("Download");
-		down.setBounds(550,500,200,100);
-		panel.add(down);
-		up.addActionListener(this);
-		down.addActionListener(this);
-		getContentPane().add(panel);
-	//192.168.1.100
-		try{
-			clientSocket = new Socket("localhost", 3333);
-			inFromServer = clientSocket.getInputStream();
-			pw = new PrintWriter(clientSocket.getOutputStream(),true);
-			outToServer = clientSocket.getOutputStream();
-		}
-		catch(Exception exc){
-			System.out.println("Exception: "+exc.getMessage());
-		}
-	}
-	public void actionPerformed(ActionEvent event){
-		if(event.getSource() == up){
-			try{
-				name = txt.getText();
-				pw.println(name);
-				//System.out.println("Upload begins");
-				//name = "c:/FTP CLIENT DIRECTORY/"+name;
-				//FileInputStream fileIn = new FileInputStream(name);
-				/*byte[] data = new byte[size];
-				int bytes = 0;
-				while((bytes = fileIn.read(data)) != -1){
-					outToServer.write(data,0,bytes);
-				}
-				fileIn.close();
-				System.out.println("Completed");*/
-
-				FileInputStream file = null;
-		        BufferedInputStream bis = null;
-		        boolean fileExists = true;
-		        name = "C:/FTP CLIENT DIRECTORY/"+name;
-		        //System.out.println(filename);
-		        try{
-		          file = new FileInputStream(name);
-		          bis = new BufferedInputStream(file);
-		        }
-		        catch(FileNotFoundException excep){
-		          fileExists = false;
-		          System.out.println("FileNotFoundException:"+excep.getMessage());
-		        }
-		        if(fileExists){
-		          System.out.println("Upload begins");
-		          sendBytes(bis,outToServer);
-		          System.out.println("Completed");
-		          bis.close();
-		          file.close();
-		          outToServer.close();
-		        }
-			}
-			catch(Exception exc){
-				System.out.println("Exception: "+exc.getMessage());
-			}
-		}
-		if(event.getSource() == down){
-			try{
-				File directory = new File(dirName);
-				if(!directory.exists()){
-					directory.mkdir();
-				}
-				//int j = 0;
-				boolean complete = true;
-				byte [] data  = new byte [size];
-				name = txt.getText();
-				file = new String("*"+name+"*");
-				pw.println(file); //lets the server know which file is to be downloaded
-				File f = new File(directory,name);
-				FileOutputStream fileOut = new FileOutputStream(f);
-				DataOutputStream dataOut = new DataOutputStream(fileOut);
-				//empty file case
-				while(complete){
-					c = inFromServer.read(data,0,data.length);
-					if(c == -1){
-						complete =  false;
-						System.out.println("Completed");
-					}
-
-					else{
-						dataOut.write(data,0,c);
-			    		dataOut.flush();
-					}
-				}
-				fileOut.close();
-			}
-			catch(Exception exc){
-				System.out.println("Exception: "+exc.getMessage());
-			}
-		}
-	}
-
-	private static void sendBytes(BufferedInputStream in, OutputStream out)throws Exception{
-    int size = 9022386;
-    byte[] data = new byte[size];
-    int bytes = 0;
-    int c = in.read(data,0,data.length);
-    out.write(data,0,c);
-    out.flush();
+class TCPClient extends JFrame implements ActionListener {
+ JPanel panel;
+ JLabel title, subT, msg;
+ Font font;
+ JTextField txt;
+ JButton up, down;
+ String dirName = "c:/FTP CLIENT DIRECTORY";
+ Socket clientSocket;
+ InputStream inFromServer;
+ OutputStream outToServer;
+ BufferedInputStream bis;
+ PrintWriter pw;
+ String name, file;
+ int c;
+ int size = 9022386;
+ public TCPClient() {
+  super("TCP CLIENT");
+  panel = new JPanel(null);
+  font = new Font("Roboto", Font.BOLD, 60);
+  title = new JLabel("TCP CLIENT");
+  title.setFont(font);
+  title.setBounds(300, 50, 400, 50);
+  panel.add(title);
+  subT = new JLabel("Enter File Name :");
+  subT.setBounds(100, 150, 200, 50);
+  panel.add(subT);
+  txt = new JTextField();
+  txt.setBounds(400, 150, 500, 50);
+  panel.add(txt);
+  up = new JButton("Upload");
+  up.setBounds(250, 250, 200, 50);
+  panel.add(up);
+  down = new JButton("Download");
+  down.setBounds(550, 250, 200, 50);
+  panel.add(down);
+  up.addActionListener(this);
+  down.addActionListener(this);
+  getContentPane().add(panel);
+  try {
+   clientSocket = new Socket("192.168.1.101", 3333);
+   inFromServer = clientSocket.getInputStream();
+   pw = new PrintWriter(clientSocket.getOutputStream(), true);
+   outToServer = clientSocket.getOutputStream();
+  } catch (Exception exc) {
+   System.out.println("Exception: " + exc.getMessage());
   }
-	
-	//ClientSocket.close();
-	public static void main(String args[]){
-		TCPClient tcp = new TCPClient();
-		tcp.setSize(1000,700);
-		tcp.setVisible(true);
-	}
+ }
+ public void actionPerformed(ActionEvent event) {
+  if (event.getSource() == up) {
+   try {
+    name = txt.getText();
+    pw.println(name);
+    FileInputStream file = null;
+    BufferedInputStream bis = null;
+    boolean fileExists = true;
+    name = "C:/FTP CLIENT DIRECTORY/" + name;
+    try {
+     file = new FileInputStream(name);
+     bis = new BufferedInputStream(file);
+    } catch (FileNotFoundException excep) {
+     fileExists = false;
+     System.out.println("FileNotFoundException:" + excep.getMessage());
+    }
+    if (fileExists) {
+     msg = new JLabel("Upload begins");
+     msg.setBounds(300, 350, 400, 50);
+     panel.add(msg);
+     //System.out.println("Upload begins");
+     sendBytes(bis, outToServer);
+     System.out.println("Completed");
+     bis.close();
+     file.close();
+     outToServer.close();
+    }
+   } catch (Exception exc) {
+    System.out.println("Exception: " + exc.getMessage());
+   }
+  }
+  if (event.getSource() == down) {
+   try {
+    File directory = new File(dirName);
+    if (!directory.exists()) {
+     directory.mkdir();
+    }
+    boolean complete = true;
+    byte[] data = new byte[size];
+    name = txt.getText();
+    file = new String("*" + name + "*");
+    pw.println(file); //lets the server know which file is to be downloaded
+    File f = new File(directory, name);
+    FileOutputStream fileOut = new FileOutputStream(f);
+    DataOutputStream dataOut = new DataOutputStream(fileOut);
+    //empty file case
+    while (complete) {
+     c = inFromServer.read(data, 0, data.length);
+     if (c == -1) {
+      complete = false;
+      System.out.println("Completed");
+     } else {
+      dataOut.write(data, 0, c);
+      dataOut.flush();
+     }
+    }
+    fileOut.close();
+   } catch (Exception exc) {
+    System.out.println("Exception: " + exc.getMessage());
+   }
+  }
+ }
+
+ private static void sendBytes(BufferedInputStream in , OutputStream out) throws Exception {
+  int size = 9022386;
+  byte[] data = new byte[size];
+  int bytes = 0;
+  int c = in .read(data, 0, data.length);
+  out.write(data, 0, c);
+  out.flush();
+ }
+ public static void main(String args[]) {
+  TCPClient tcp = new TCPClient();
+  tcp.setSize(1000, 900);
+  tcp.setVisible(true);
+ }
 }
--- TCPServer.java
+++ TCPServer.java
@@ -1,112 +1,107 @@
 import java.io.*;
 import java.net.*;
 
-public class TCPServer{
-  public static void main(String args[]) throws Exception{
-    int id = 1;
-    System.out.println("Server started...");
-    System.out.println("Waiting for connections...");
-    ServerSocket welcomeSocket = new ServerSocket(3333);
-    while(true){
-      Socket connectionSocket = welcomeSocket.accept();
-      System.out.println("Client with ID "+id+" connected from "+connectionSocket.getInetAddress().getHostName()+"..." );
-      Thread server = new ThreadedServer(connectionSocket,id);
-      id++;
-      server.start();
-    }
+public class TCPServer {
+ public static void main(String args[]) throws Exception {
+  int id = 1;
+  System.out.println("Server started...");
+  System.out.println("Waiting for connections...");
+  ServerSocket welcomeSocket = new ServerSocket(3333);
+  while (true) {
+   Socket connectionSocket = welcomeSocket.accept();
+   System.out.println("Client with ID " + id + " connected from " + connectionSocket.getInetAddress().getHostName() + "...");
+   Thread server = new ThreadedServer(connectionSocket, id);
+   id++;
+   server.start();
   }
+ }
 }
 
-class ThreadedServer extends Thread{
-  int n;
-  int m;
-  String name, f, ch, fileData;
-  String filename;
-  Socket connectionSocket;
-  int counter;
-  String dirName = "c:/FTP SERVER DIRECTORY";
-  public ThreadedServer(Socket s,int c){
-    connectionSocket = s;
-    counter = c;
-  }
-  public void run(){
-    try{
-      BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-      InputStream inFromClient = connectionSocket.getInputStream();
-      PrintWriter out = new PrintWriter(connectionSocket.getOutputStream());
-      OutputStream output = connectionSocket.getOutputStream();
-      name = in.readLine();
-      //System.out.println(name);
-      ch = name.substring(0,1);
-      if(ch.equals("*")){
-        n = name.lastIndexOf("*");
-        filename = name.substring(1,n);
-        FileInputStream file = null;
-        BufferedInputStream bis = null;
-        boolean fileExists = true;
-        System.out.println("Request to download file "+filename+" recieved from "+connectionSocket.getInetAddress().getHostName()+"...");
-        filename = "C:/FTP SERVER DIRECTORY/"+filename;
-        //System.out.println(filename);
-        try{
-          file = new FileInputStream(filename);
-          bis = new BufferedInputStream(file);
-        }
-        catch(FileNotFoundException excep){
-          fileExists = false;
-          System.out.println("FileNotFoundException:"+excep.getMessage());
-        }
-        if(fileExists){
-          System.out.println("Download begins");
-          sendBytes(bis,output);
-          System.out.println("Completed");
-          bis.close();
-          file.close();
-          output.close();
-        }
-      }
-      else{
-        try{
-          boolean complete = true;
-          System.out.println("Request to upload file " +name+" recieved from "+connectionSocket.getInetAddress().getHostName()+"...");
-          File directory = new File(dirName);
-          if(!directory.exists()){
-            System.out.println("Dir made");
-            directory.mkdir();
-          }
-          int size = 9022386;
-          byte [] data  = new byte [size];
-          File fc = new File(directory, name);
-          FileOutputStream fileOut = new FileOutputStream(fc);
-          DataOutputStream dataOut = new DataOutputStream(fileOut);
-          while(complete){
-            m = inFromClient.read(data,0,data.length);
-            if(m == -1){
-              complete =  false;
-              System.out.println("Completed");
-            }
-            else{
-              dataOut.write(data,0,m);
-              dataOut.flush();
-            }
-          }
-          fileOut.close();
-        }
-        catch(Exception exc){
-          System.out.println(exc.getMessage());
-        }
-      }
-      connectionSocket.close();
+class ThreadedServer extends Thread {
+ int n;
+ int m;
+ String name, f, ch, fileData;
+ String filename;
+ Socket connectionSocket;
+ int counter;
+ String dirName = "c:/FTP SERVER DIRECTORY";
+ public ThreadedServer(Socket s, int c) {
+  connectionSocket = s;
+  counter = c;
+ }
+ public void run() {
+  try {
+   BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+   InputStream inFromClient = connectionSocket.getInputStream();
+   PrintWriter out = new PrintWriter(connectionSocket.getOutputStream());
+   OutputStream output = connectionSocket.getOutputStream();
+   name = in .readLine();
+   //System.out.println(name);
+   ch = name.substring(0, 1);
+   if (ch.equals("*")) {
+    n = name.lastIndexOf("*");
+    filename = name.substring(1, n);
+    FileInputStream file = null;
+    BufferedInputStream bis = null;
+    boolean fileExists = true;
+    System.out.println("Request to download file " + filename + " recieved from " + connectionSocket.getInetAddress().getHostName() + "...");
+    filename = "C:/FTP SERVER DIRECTORY/" + filename;
+    //System.out.println(filename);
+    try {
+     file = new FileInputStream(filename);
+     bis = new BufferedInputStream(file);
+    } catch (FileNotFoundException excep) {
+     fileExists = false;
+     System.out.println("FileNotFoundException:" + excep.getMessage());
     }
-    catch(Exception ex){
-      System.out.println(ex.getMessage());
+    if (fileExists) {
+     System.out.println("Download begins");
+     sendBytes(bis, output);
+     System.out.println("Completed");
+     bis.close();
+     file.close();
+     output.close();
     }
+   } else {
+    try {
+     boolean complete = true;
+     System.out.println("Request to upload file " + name + " recieved from " + connectionSocket.getInetAddress().getHostName() + "...");
+     File directory = new File(dirName);
+     if (!directory.exists()) {
+      System.out.println("Dir made");
+      directory.mkdir();
+     }
+     int size = 9022386;
+     byte[] data = new byte[size];
+     File fc = new File(directory, name);
+     FileOutputStream fileOut = new FileOutputStream(fc);
+     DataOutputStream dataOut = new DataOutputStream(fileOut);
+     while (complete) {
+      m = inFromClient.read(data, 0, data.length);
+      if (m == -1) {
+       complete = false;
+       System.out.println("Completed");
+      } else {
+       dataOut.write(data, 0, m);
+       dataOut.flush();
+      }
+     }
+     fileOut.close();
+    } catch (Exception exc) {
+     System.out.println(exc.getMessage());
+    }
+   }
+   connectionSocket.close();
+  } catch (Exception ex) {
+   System.out.println(ex.getMessage());
   }
-  private static void sendBytes(BufferedInputStream in, OutputStream out)throws Exception{
-    int size = 9022386;
-    byte[] data = new byte[size];
-    int bytes = 0;
-    int c = in.read(data,0,data.length);
-    out.write(data,0,c);
-    out.flush();
-  }
+ }
+ private static void sendBytes(BufferedInputStream in , OutputStream out) throws Exception {
+  int size = 9022386;
+  byte[] data = new byte[size];
+  int bytes = 0;
+  int c = in .read(data, 0, data.length);
+  out.write(data, 0, c);
+  out.flush();
+ }
 }
